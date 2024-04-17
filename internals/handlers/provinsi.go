8bacc7e3d0055e0176413d8d776dd65da4de510b
Musbahniar <musbahniar.mnr@gmail.com>
2024-04-17 08:14:56 +0700

    feat: swagger

--- internals/handlers/provinsi.go
+++ internals/handlers/provinsi.go
@@ -2,6 +2,17 @@ package handler
 
 import "github.com/gofiber/fiber/v2"
 
+// GetOrderByCode Getting Order by Code
+//
+//	@Summary		Getting Order by Code
+//	@Description	Getting Order by Code in detail
+//	@Tags			Orders
+//	@Accept			json
+//	@Produce		json
+//	@Param			x-correlationid	header		string	true	"code of Order"
+//	@Param			orderCode		path		string	true	"code of Order"
+//	@Success		200				{string}	string
+//	@Router			/orders/code/{orderCode} [get]
 func GetProvinsi(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{"status": "success", "message": "Provinsi Found", "data": ""})
 }
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	_ "github.com/Musbahniar/golang-project-struktur/docs"
 	"github.com/Musbahniar/golang-project-struktur/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 )
 
+// @title			Belajar Resful Api
+// @version		1.0
+// @description	Documentation for Belajar Resful Api
+// @termsOfService	http://swagger.io/terms/
 func main() {
 	// Start a new fiber app
 	app := fiber.New()
+
+	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	// Health check
 	app.Get("/", func(c *fiber.Ctx) error {
